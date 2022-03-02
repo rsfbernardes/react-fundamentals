d6b8e0cafaf1a55fa13db2440d18003a11825006
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-01 23:25:48 -0300

    config webpack loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import react from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
     <h1 id="title">
-        <span>Hello World!</span>
+        <span>Hello World!!!</span>
         </h1>,
     document.getElementById('root'),
-)
+);
