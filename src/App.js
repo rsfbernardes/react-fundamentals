5d323a75147ae4d33bf1cb017b4148dcf90f1d47
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-02 21:56:52 -0300

    React props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Post from "./Post";
+
 const category = 'Posts Of The Week'
 
 function App() {
@@ -7,8 +9,29 @@ function App() {
     <>
       <h1>JStack's Blog</h1>
       <h2>{category}</h2>
+
+      <hr />
+
+      <Post 
+        post={{
+          title: 'Title 01',
+          subtitle: 'Subtitle 01'
+        }}
+      />
+      <Post 
+        post={{
+          title: 'Title 02',
+          subtitle: 'Subtitle 02'
+        }}
+      />
+      <Post 
+        post={{
+          title: 'Title 03',
+          subtitle: 'Subtitle 03'
+        }}
+      />
     </>
-  )
+  );
 };
 
 export default App;
