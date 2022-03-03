aa48bd9b229423880590448a329f05510d764dae
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-03 20:29:14 -0300

    Config Prop-Types

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from "react";
 
 import Post from "./Post";
-import Header from "./Head";
+import Header from "./Header";
 
 function App() {
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Posts Of The Week</h2>
+      <h2>Posts Of The Week</h2>
       </Header>
 
       <hr />
 
       <Post 
+        likes={20}
         post={{
           title: 'News Title 01',
           subtitle: 'News Subtitle 01'
         }}
       />
       <Post 
+        likes={10}
         post={{
           title: 'News Title 02',
           subtitle: 'News Subtitle 02'
         }}
       />
       <Post 
+        likes={50}
         post={{
           title: 'News Title 03',
           subtitle: 'News Subtitle 03'
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function Header(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      {props.children}
+    </>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+}
