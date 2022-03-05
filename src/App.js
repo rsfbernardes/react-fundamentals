528a4cffc891c54f34b753b04e1b4d2dd624ca15
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-05 12:26:00 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-  {title: 'Title#01', subtitle: 'Sub#01', likes: 20},
-  {title: 'Title#02', subtitle: 'Sub#02', likes: 10},
-  {title: 'Title#03', subtitle: 'Sub#03', likes: 50},
-  {title: 'Title#04', subtitle: 'Sub#04', likes: 50},
-];
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 50 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50 },
+  ]);
+
+  console.log({ posts });
+
+  function handleInsert() {
+    // posts.push({
+    //   
+    // })
+
+    setTimeout(() => {
+      setPosts((prevState => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 50,
+        },
+      ]));
+    }, 2000);
+  }
+
   return (
     <>
       <Header>
-      <h2>Posts Of The Week</h2>
+        <h2>
+          Posts Of The Week
+          <button onClick={handleInsert}>Insert</button>
+        </h2>
       </Header>
 
       <hr />
 
       {posts.map(post => (
-        <Post 
-          key={post.title}
+        <Post
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
