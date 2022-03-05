bd319e033af5dc89d5c5d25f1463be3cbf8654cd
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-05 19:33:33 -0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Header from "./Header";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 50 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50 },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 50, read: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true },
   ]);
 
   function handleInsert() {
@@ -47,11 +47,7 @@ function App() {
           key={post.id}
           likes={post.likes}
           onRemove={handleRemovePost}
-          post={{
-            id: post.id,
-            title: post.title,
-            subtitle: post.subtitle,
-          }}
+          post={post}
         />
       ))}
     </>
--- src/Post.js
+++ src/Post.js
@@ -2,10 +2,17 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export default function Post(props) {
+  // if you want to alter the structure of your page, use conditional rendering this way
+  // if(props.post.read) {
+  //   return <h2>{props.post.title} already read</h2>
+  // }
   return (
     <>
       <article>
-        <strong>{props.post.title}</strong>
+        {/* one simple way to use conditional rendering:
+        { props.post.read ? <s>{props.post.title}</s> : <strong>{props.post.title}</strong> } */}
+        {props.post.read && <s>{props.post.title}</s>}
+        {!props.post.read && props.post.title}
         <button onClick={() => props.onRemove(props.post.id)}>Remove</button>
         <br />
         <small>{props.post.subtitle}</small>
@@ -18,11 +25,12 @@ export default function Post(props) {
 };
 
 Post.propTypes = {
-  likes: PropTypes.number.isRequired,
   onRemove: PropTypes.func.isRequired,
   post: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired
+    subtitle: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    read: PropTypes.bool.isRequired,
   }).isRequired,
 }
