a9527d69001462e26ba8b21aaad4d91f51d6dfe0
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-06 20:46:25 -0300

    Inline Style on Header.js

--- src/Header.js
+++ src/Header.js
@@ -8,13 +8,17 @@ export default function Header(props) {
   const {onToogleTheme} = useContext(ThemeContext);
 
   return (
-    <>
+    <div 
+      style={{
+        background: '#ccc',
+        paddingLeft: '5px',
+      }}>
       <h1>{props.title}</h1>
       <Button onClick={onToogleTheme}>
         Change Theme
       </Button>
       {props.children}
-    </>
+    </div>
   );
 };
 
