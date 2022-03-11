0e80bcc5899004cd9781bab843bffafb7c8be253
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-10 23:15:30 -0300

    Working with props in Styled Components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import PostHeader from "./PostHeader";
 
-import { Subtitle, Average } from "./styles";
+import { Container, Subtitle, Average } from "./styles";
 
 export default function Post(props) {
   return (
       <>
-        <article>
+        <Container removed={props.post.removed}>
         <PostHeader onRemove={props.onRemove}
           post={{
             id: props.post.id,
@@ -18,7 +18,7 @@ export default function Post(props) {
 
         <Subtitle>{props.post.subtitle}</Subtitle>
         <Average>Media: {props.likes / 2}</Average>
-      </article>
+      </Container>
       </>
   )
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
+export const Container = styled.article`
+  opacity: ${(props) => props.removed ? 0.5 : 1}
+`
+
 export const Subtitle = styled.small`
   display: block;
 `
-
 export const Average = styled.span`
   font-size: 12px;
   opacity: 0.7;
