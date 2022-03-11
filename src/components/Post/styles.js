4dc5a21371e89fc4c1c37fac21f87a742fc9e862
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-03-10 23:30:04 -0300

    Defining many styles into the same function

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,7 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.article`
-  opacity: ${(props) => props.removed ? 0.5 : 1}
+  /* opacity: ${(props) => props.removed ? 0.5 : 1};
+  color: ${(props) => props.removed ? '#F00' : '#FFF'}; */
+
+  ${(props) => css`
+            opacity: ${props.removed ? 0.5 : 1};
+            color: ${props.removed ? '#F00' : '#FFF'};
+  `}
 `
 
 export const Subtitle = styled.small`
